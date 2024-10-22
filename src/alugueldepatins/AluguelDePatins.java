0a94af28a2028ebb445cba6f7e11fa4a5e712f4a
AnaDamiao <ana.damiao@iob.com.br>
2024-10-21 23:28:26 -0300

    first commit

---
+++ src/alugueldepatins/AluguelDePatins.java
@@ -0,0 +1,16 @@
+package alugueldepatins;
+
+import Home.Home;
+
+/**
+ *
+ * @author AnaDamião
+ */
+public class AluguelDePatins {
+
+    public static void main(String[] args) {
+       Home homeAluguel = new Home();
+       homeAluguel.setVisible(true);
+    }
+    
+}
